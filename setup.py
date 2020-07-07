2b00ad8c35eacbeafca28c6ac6675ac8a900433e
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-07 02:44:40 +0200

    lib

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='seq_match_corrector',
+    version='0.1',
+    description='SequenceMatcher based corrector',
+    url='https://github.com/slntopp/seq_match_corrector',
+    author='slntopp',
+    author_email='info@slnt-opp.xyz',
+    license='Apache-2.0 License',
+    packages=['seq_match_corrector'],
+    zip_safe=False)
