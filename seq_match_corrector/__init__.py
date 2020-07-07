31ba7e07397730543d36a4da09cff4a4ac3d1c0e
Mikita Iwanowski <info@slnt-opp.xyz>
2020-07-08 01:11:00 +0200

    len range fix

--- seq_match_corrector/__init__.py
+++ seq_match_corrector/__init__.py
@@ -2,15 +2,16 @@ from difflib import SequenceMatcher
 
 def ratios(target: list, dictionary: list) -> list:
     def ratio(word: str):
+        if not word: return
         l = len(word)
         return sorted(
             filter(
                 lambda x: x[1] > 0.5,
-                map(lambda x: [x, SequenceMatcher(None, x, word).ratio()], filter(lambda x: len(x) in range(l - 1, l + 1), dictionary))
+                map(lambda x: [x, SequenceMatcher(None, x, word).ratio()], filter(lambda x: len(x) in range(l - 2, l + 3), dictionary))
             ), key=lambda x: x[1], reverse=True
         )
     return list(
-        map(ratio, target)
+        filter(lambda x: x, map(ratio, target))
     )
 
 def correct(target: list, dictionary: list) -> dict:
